85a5e59118c2d21c02e3679d7fcfbd4646a61bad
Basith P <basithpofficial@gmail.com>
2021-09-15 16:52:40 +0530

    Fix const keyword errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'screens/home_screen.dart';
 
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Portfolio',
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Center(child: Text('Hello'))));
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
   }
 }
