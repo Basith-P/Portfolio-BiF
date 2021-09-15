b3ffe45bda6f76b7527045cf277c654e814e9ea0
Basith P <basithpofficial@gmail.com>
2021-09-15 16:34:43 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/portfolio_bif/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portfolio_bif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      home: HomeScreen(),
+    );
+  }
+}
